84acdac99605438a4ec5f0aa324b5935051ac447
yangyxu <yangyxu@cisco.com>
2021-05-23 09:40:26 +0800

    rebuild

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ if(!global.zn){
     require('@zeanium/core');
 }
 
-if(process.env.NODE_ENV == 'development'){
-    var node_path = require('path');
-    var node_fs = require('fs');
-    var _version = '';
-    if(node_fs.existsSync(node_path.join(__dirname, './package.json'))){
-        _version = require(node_path.join(__dirname, './package.json')).version;
-    }
-    zn.debug('server_path', (_version?'[ ' + _version + ' ]':''), ': ', __dirname);
+var node_path = require('path');
+var node_fs = require('fs');
+var _version = '';
+if(node_fs.existsSync(node_path.join(__dirname, './package.json'))){
+    _version = require(node_path.join(__dirname, './package.json')).version;
 }
+zn.info('server_path', (_version?'[ ' + _version + ' ]':''), ': ', __dirname);
 
 module.exports = require('./src/_index.js');
